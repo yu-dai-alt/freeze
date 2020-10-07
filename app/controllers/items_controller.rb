6a2f61de64f8040539353b89cd8a4424fa5c4492
taroimocup <66675162+taroimocup@users.noreply.github.com>
2020-10-07 20:27:46 +0900

    基本ルーティング設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,41 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show]
+
   def index
     @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
+  def show
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :image, :text)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
